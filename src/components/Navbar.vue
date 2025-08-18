f52c728087db848780abbfe288aaa5717768d06b
Davi <ifbadavi@gmail.com>
2025-08-18 18:56:09 -0300

    fix: atualizando forma de download do curriculo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,7 +24,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
             </svg>
           </button>
-          <a href="../../public/cv.pdf" download>
+          <a href="https://raw.githubusercontent.com/DaviRamosUC/davilima.co/main/public/cv.pdf">
             <button class="bg-black dark:bg-white text-white dark:text-black px-4 py-2 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-200 transition-colors">
               Curriculum Vitae
             </button>
